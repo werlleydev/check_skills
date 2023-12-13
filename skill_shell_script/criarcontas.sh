ea8cd4606a05d9c6a1451c8c96eb72fe0be0e6fc
Jonhnny Werlley <jonhnny.werlley.s.s@gmail.com>
2023-12-13 20:21:59 -0300

    refactor: aplica sugestoes do shellcheck

--- skill_shell_script/criarcontas.sh
+++ skill_shell_script/criarcontas.sh
@@ -2,11 +2,11 @@
 
 set -eou pipefail
 
-for username in $(cat novosusuarios | tr ":" "\n")
+for username in $(tr ":" "\n" < novosusuarios)
 do
   sre_username="sre_$username" 
   echo "Create user $sre_username"
-  useradd -m -s /bin/bash $sre_username
+  useradd -m -s /bin/bash "$sre_username"
   echo "$sre_username  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 done
 
